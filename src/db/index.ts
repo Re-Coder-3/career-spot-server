a751c469281304f66494a4a2793a657534ecc970
ehddnr301 <dy950328@gmail.com>
2020-07-23 11:31:01 +0900

    타입추가, sequelize버전변경

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,10 @@
 import { Sequelize, Model, DataTypes, NOW } from "sequelize";
 import { config } from "./config";
 
-
 const db = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: "mysql",
-  operatorsAliases: false,
+  // operatorsAliases: false,
   pool: {
     max: 5,
     min: 0,
@@ -18,31 +17,37 @@ const db = new Sequelize(config.database, config.username, config.password, {
 });
 
 db.authenticate()
-    .then(() => {
-      console.log("✅ Connection successfully.");
-    })
-    .catch((err) => {
-      console.error("❌ Unable to connect to the database:", err);
-    });
+  .then(() => {
+    console.log("✅ Connection successfully.");
+  })
+  .catch((err) => {
+    console.error("❌ Unable to connect to the database:", err);
+  });
 
 /**
  * 0721 - 유진
  * 프라이머리 키를 꼭 지정해줘야한다.
  * 지정 안해주면 자동으로 id 필드명이 들어가서 Unknown column ~ 오류 발생
  */
-export const category = db.define("category", {
-  category_idx: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, unique: true, allowNull: true },
-  category_name: { type: Sequelize.STRING },
-});
+// export const category = db.define("category", {
+//   category_idx: {
+//     type: Sequelize.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//     unique: true,
+//     allowNull: true,
+//   },
+//   category_name: { type: Sequelize.STRING },
+// });
 
-export const user = db.define("user", {
-  user_idx: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    defaultValue: NOW,
-  },
-  user_name: { type: Sequelize.STRING },
-  user_email: { type: Sequelize.STRING },
-  user_password: { type: Sequelize.STRING },
-});
+// export const user = db.define("user", {
+//   user_idx: {
+//     type: Sequelize.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//     defaultValue: NOW,
+//   },
+//   user_name: { type: Sequelize.STRING },
+//   user_email: { type: Sequelize.STRING },
+//   user_password: { type: Sequelize.STRING },
+// });
