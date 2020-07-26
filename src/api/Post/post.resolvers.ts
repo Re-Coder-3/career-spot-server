a21725fd61175912a54c1068ac8a0de74d248ac8
2ujin <suddenlydisappear200@gmail.com>
2020-07-26 17:10:10 +0900

    createPost 뮤테이션 샏성

--- src/api/Post/post.resolvers.ts
+++ src/api/Post/post.resolvers.ts
@@ -5,11 +5,22 @@ export default {
     findPost: async (_: any, args: any) => {
       try {
         const result = await Post.findAndCountAll({});
-        console.log(result)
         return result.rows;
       } catch {
         return false;
       }
+    },
+  },
+  Mutation: {
+    createPost: async (_:any, args: any) => {
+      console.log(args)
+      // try{
+      //   result = await Category.create(args);
+      // }catch(e){
+      //   console.log(e)
+      //   throw e
+      // }
+      return args;
     },
   }
 };
