32b5975c6aba8d149d8d399e620c2e8acac8963e
ehddnr301 <dy950328@gmail.com>
2020-07-30 19:57:37 +0900

    [#12] Kakao auth 구현

---
+++ src/api/User/SocialAuth.resolvers.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import { KakaoAuthMutationArgs } from '../../types/graph';
+import { User, Image } from '../../db/index';
+
+export default {
+  Mutation: {
+    kakaoAuth: async (_: any, args: KakaoAuthMutationArgs) => {
+      const SECRET_KEY = process.env.JWT_SECRET_KEY!;
+      const { code } = args;
+      try {
+        const response = await axios.get(
+          `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_KEY}&redirect_uri=${process.env.KAKAO_CALLBACK}&code=${code}&client_secret=${process.env.KAKAO_SECRET}`,
+        );
+        const {
+          data: { access_token },
+        } = response;
+        const profileRequest = await axios.get('https://kapi.kakao.com/v2/user/me', {
+          headers: { Authorization: `Bearer ${access_token}` },
+        });
+        const {
+          data: {
+            properties: { nickname },
+            kakao_account: {
+              email,
+              profile: {
+                profile_image_url = 'https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png',
+              },
+            },
+          },
+        } = profileRequest;
+        if (email) {
+          const user = await User.findOne({ where: { user_email: email } });
+          if (user) {
+            const jwtToken = jwt.sign({ user }, SECRET_KEY, {
+              expiresIn: '4h',
+            });
+            return jwtToken;
+          } else {
+            const image_result = await Image.create({
+              image_idx: '',
+              image_url: profile_image_url,
+            });
+            const newUser = await User.create({
+              user_idx: '',
+              user_name: nickname,
+              user_email: email,
+              user_profile_image: image_result.image_idx,
+              user_password: '',
+            });
+            const jwtToken = jwt.sign({ newUser }, SECRET_KEY, {
+              expiresIn: '4h',
+            });
+            return jwtToken;
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,10 +1,11 @@
-export const typeDefs = ["type Query {\n  findCategory: [Category]!\n  findPost: returnType!\n  findScrap: [Scrap]!\n  findAllUser: UserReturnType!\n  checkUser: String\n}\n\ntype Mutation {\n  createCategory(category_idx: Int!, category_name: String!): Category!\n  deleteCategory(category_idx: Int!): Category!\n  updateCategory(category_idx: Int!, category_name: String!): Category!\n  createPost(image: image!, post: post!): Post!\n  createScrap(scrap: scrap!): Scrap!\n  createUser(user_name: String!, user_email: String!, user_password: String!): String!\n  loginUser(user_email: String!, user_password: String!): String!\n  deleteUser(user_idx: Int!): User!\n  updateUser(image_url: String): String!\n}\n\ntype Category {\n  category_idx: Int!\n  category_name: String!\n}\n\ntype Post {\n  post_idx: Int\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n  category: Category!\n  user: User!\n  image: Image!\n}\n\ninput image {\n  image_url: String\n}\n\ninput post {\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n}\n\ntype returnType {\n  count: Int!\n  rows: [Post!]!\n  count: Int!\n  rows: [Post!]!\n}\n\ntype Image {\n  image_idx: Int!\n  image_url: String!\n}\n\ntype Scrap {\n  scrap_idx: Int!\n  user_idx: Int!\n  post_idx: Int!\n}\n\ninput scrap {\n  user_idx: Int!\n  post_idx: Int!\n}\n\ntype UserReturnType {\n  count: Int!\n  rows: [User!]!\n}\n\ntype User {\n  user_idx: Int!\n  user_name: String!\n  user_email: String!\n  user_password: String!\n}\n"];
+export const typeDefs = ["type Query {\n  findCategory: [Category]!\n  findPost: returnType!\n  findScrap: [Scrap]!\n  findScrapForUser: PostreturnType!\n  findAllUser: UserReturnType!\n  checkUser: String\n}\n\ntype Mutation {\n  createCategory(category_idx: Int!, category_name: String!): Category!\n  deleteCategory(category_idx: Int!): Category!\n  updateCategory(category_idx: Int!, category_name: String!): Category!\n  createPost(image: image!, post: post!): Post!\n  createScrap(scrap: scrap!): Scrap!\n  kakaoAuth(code: String!): String!\n  createUser(user_name: String!, user_email: String!, user_password: String!): String!\n  loginUser(user_email: String!, user_password: String!): String!\n  deleteUser(user_idx: Int!): User!\n  updateUser(image_url: String): String!\n}\n\ntype Category {\n  category_idx: Int!\n  category_name: String!\n}\n\ntype Post {\n  post_idx: Int\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n  category: Category!\n  user: User!\n  image: Image!\n}\n\ninput image {\n  image_url: String\n}\n\ninput post {\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n}\n\ntype returnType {\n  count: Int!\n  rows: [Post!]!\n}\n\ntype Image {\n  image_idx: Int!\n  image_url: String!\n}\n\ntype Scrap {\n  scrap_idx: Int!\n  user_idx: Int!\n  post_idx: Int!\n  post: Post!\n  user: User!\n}\n\ninput scrap {\n  user_idx: Int!\n  post_idx: Int!\n}\n\ntype PostreturnType {\n  count: Int!\n  rows: [Scrap!]!\n}\n\ntype UserReturnType {\n  count: Int!\n  rows: [User!]!\n}\n\ntype User {\n  user_idx: Int!\n  user_name: String!\n  user_email: String!\n  user_password: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   findCategory: Array<Category>;
   findPost: returnType;
   findScrap: Array<Scrap>;
+  findScrapForUser: PostreturnType;
   findAllUser: UserReturnType;
   checkUser: string | null;
 }
@@ -47,6 +48,13 @@ export interface Scrap {
   scrap_idx: number;
   user_idx: number;
   post_idx: number;
+  post: Post;
+  user: User;
+}
+
+export interface PostreturnType {
+  count: number;
+  rows: Array<Scrap>;
 }
 
 export interface UserReturnType {
@@ -60,6 +68,7 @@ export interface Mutation {
   updateCategory: Category;
   createPost: Post;
   createScrap: Scrap;
+  kakaoAuth: string;
   createUser: string;
   loginUser: string;
   deleteUser: User;
@@ -87,6 +96,10 @@ export interface CreatePostMutationArgs {
 
 export interface CreateScrapMutationArgs {
   scrap: scrap;
+}
+
+export interface KakaoAuthMutationArgs {
+  code: string;
 }
 
 export interface CreateUserMutationArgs {
