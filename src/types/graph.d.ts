e6990f17fcd5b1f8f0660ffc40881c12cecdab5c
ehddnr301 <dy950328@gmail.com>
2020-08-01 14:45:07 +0900

    [#15] singleUpload

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Query {\n  findCategory: [Category]!\n  findPost: returnType!\n  findScrap: [Scrap]!\n  findScrapForUser: PostreturnType!\n  findAllUser: UserReturnType!\n  checkUser: String\n}\n\ntype Mutation {\n  createCategory(category_idx: Int!, category_name: String!): Category!\n  deleteCategory(category_idx: Int!): Category!\n  updateCategory(category_idx: Int!, category_name: String!): Category!\n  createPost(image: image!, post: post!): Post!\n  createScrap(scrap: scrap!): Scrap!\n  kakaoAuth(code: String!): String!\n  createUser(user_name: String!, user_email: String!, user_password: String!): String!\n  loginUser(user_email: String!, user_password: String!): String!\n  deleteUser(user_idx: Int!): User!\n  updateUser(image_url: String): String!\n}\n\ntype Category {\n  category_idx: Int!\n  category_name: String!\n}\n\ntype Post {\n  post_idx: Int\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n  category: Category!\n  user: User!\n  image: Image!\n}\n\ninput image {\n  image_url: String\n}\n\ninput post {\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n}\n\ntype returnType {\n  count: Int!\n  rows: [Post!]!\n}\n\ntype Image {\n  image_idx: Int!\n  image_url: String!\n}\n\ntype Scrap {\n  scrap_idx: Int!\n  user_idx: Int!\n  post_idx: Int!\n  post: Post!\n  user: User!\n}\n\ninput scrap {\n  user_idx: Int!\n  post_idx: Int!\n}\n\ntype PostreturnType {\n  count: Int!\n  rows: [Scrap!]!\n}\n\ntype UserReturnType {\n  count: Int!\n  rows: [User!]!\n}\n\ntype User {\n  user_idx: Int!\n  user_name: String!\n  user_email: String!\n  user_password: String!\n}\n"];
+export const typeDefs = ["type Query {\n  findCategory: [Category]!\n  findPost: returnType!\n  findScrap: [Scrap]!\n  findScrapForUser: PostreturnType!\n  findAllUser: UserReturnType!\n  checkUser: String\n}\n\ntype Mutation {\n  createCategory(category_idx: Int!, category_name: String!): Category!\n  deleteCategory(category_idx: Int!): Category!\n  updateCategory(category_idx: Int!, category_name: String!): Category!\n  singleUpload(file: Upload!): Boolean!\n  createPost(image: image!, post: post!): Post!\n  createScrap(scrap: scrap!): Scrap!\n  kakaoAuth(code: String!): String!\n  createUser(user_name: String!, user_email: String!, user_password: String!): String!\n  loginUser(user_email: String!, user_password: String!): String!\n  deleteUser(user_idx: Int!): User!\n  updateUser(image_url: String): String!\n}\n\ntype Category {\n  category_idx: Int!\n  category_name: String!\n}\n\nscalar Upload\n\ntype Post {\n  post_idx: Int\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n  category: Category!\n  user: User!\n  image: Image!\n}\n\ninput image {\n  image_url: String\n}\n\ninput post {\n  category_idx: Int!\n  user_idx: Int!\n  image_idx: Int!\n  post_title: String!\n  post_content: String!\n}\n\ntype returnType {\n  count: Int!\n  rows: [Post!]!\n}\n\ntype Image {\n  image_idx: Int!\n  image_url: String!\n}\n\ntype Scrap {\n  scrap_idx: Int!\n  user_idx: Int!\n  post_idx: Int!\n  post: Post!\n  user: User!\n}\n\ninput scrap {\n  user_idx: Int!\n  post_idx: Int!\n}\n\ntype PostreturnType {\n  count: Int!\n  rows: [Scrap!]!\n}\n\ntype UserReturnType {\n  count: Int!\n  rows: [User!]!\n}\n\ntype User {\n  user_idx: Int!\n  user_name: String!\n  user_email: String!\n  user_password: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -66,6 +66,7 @@ export interface Mutation {
   createCategory: Category;
   deleteCategory: Category;
   updateCategory: Category;
+  singleUpload: boolean;
   createPost: Post;
   createScrap: Scrap;
   kakaoAuth: string;
@@ -87,6 +88,10 @@ export interface DeleteCategoryMutationArgs {
 export interface UpdateCategoryMutationArgs {
   category_idx: number;
   category_name: string;
+}
+
+export interface SingleUploadMutationArgs {
+  file: Upload;
 }
 
 export interface CreatePostMutationArgs {
@@ -120,6 +125,8 @@ export interface DeleteUserMutationArgs {
 export interface UpdateUserMutationArgs {
   image_url: string | null;
 }
+
+export type Upload = any;
 
 export interface image {
   image_url: string | null;
