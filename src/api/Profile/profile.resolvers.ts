3ba4c1029f43fbc3590d6b1e908a473647e80548
ehddnr301 <dy950328@gmail.com>
2020-08-03 23:00:48 +0900

    error - type error

--- src/api/Profile/profile.resolvers.ts
+++ src/api/Profile/profile.resolvers.ts
@@ -8,29 +8,40 @@ export default {
     meProfile: async (_: any, __: any, context: Context): Promise<meProfileReturnType> => {
       const u = getUser(context);
       const userIdx = u?.user_idx;
-      const user = await User.findOne({
-        include: [
-          {
-            model: Profile,
-            include: [
-              {
-                model: Image,
-              },
-              {
-                model: Category,
-              },
-            ],
-          },
+      const profile = await Profile.findOne({
+        attributes: [
+          'profile_idx',
+          'user_idx',
+          'user_name',
+          'user_location',
+          'user_education',
+          'user_profile_img',
+          'user_like_category_idx',
+          'user_career',
         ],
         where: {
           user_idx: userIdx,
         },
-        raw: true,
+        include: [
+          {
+            model: Image,
+            required: true,
+          },
+          {
+            model: Category,
+            required: true,
+          },
+          {
+            model: User,
+            required: true,
+          },
+        ],
       });
-      console.log(user);
+      console.log(profile);
+
       return {
         status: 200,
-        data: null,
+        data: profile,
         error: null,
       };
     },
